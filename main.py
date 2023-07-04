6231dba6c7c86b817c46ffba837c2f22bd2f15bc
Lukas Krimphove <lukas.krimphove@gmx.de>
2023-07-04 12:58:52 +0200

    feat: added possibility to add root_path

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ import requests
 CURRENTLY_PLAYING_ENDPOINT = 'https://www.antenne.de/api/metadata/now'
 RELEVANT_DATA = ['artist', 'title', 'isrc', 'starttime', 'mountpoint']
 REFRESH_RATE = 90  # seconds
+ROOT_PATH = ""
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG,
-                    filename='app.log',
+                    filename=f'{ROOT_PATH}app.log',
                     filemode='a',
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%d-%m-%y %H:%M:%S')
@@ -26,7 +27,7 @@ def read_relevant_stations():
     # read in stations to monitor
     # only songs from stations listed in relevant_stations.txt will be tracked
     # leave file empty to monitor every station
-    file_path = 'relevant_stations.txt'
+    file_path = f'{ROOT_PATH}relevant_stations.txt'
 
     relevant_stations = []
     if os.path.isfile(file_path):
@@ -54,7 +55,7 @@ def write_to_parquet():
 
     now = datetime.now()
     file_name = f'{now.date()}.parquet'
-    directory = f'gathered_data/{now.year}/{now.month}'
+    directory = f'{ROOT_PATH}gathered_data/{now.year}/{now.month}'
     file_path = f'{directory}/{file_name}'
 
     df = pd.DataFrame(records)
@@ -83,7 +84,7 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 
-if __name__ == '__main__':
+def main():
     logger.info('Start')
 
     signal.signal(signal.SIGINT, signal_handler)
@@ -117,6 +118,7 @@ if __name__ == '__main__':
                         new_songs[station] = currently_playing[station]
                         continue
         playing = currently_playing
+        logger.info(f'{len(new_songs)} new songs')
         logger.debug(f'New songs playing: {new_songs}')
 
         for station in new_songs:
@@ -128,3 +130,10 @@ if __name__ == '__main__':
             records = []
 
         time.sleep(REFRESH_RATE)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
